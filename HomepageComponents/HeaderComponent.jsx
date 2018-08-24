b60ca9f7257eddc044217aa3a7cc0223cc309aa7
nicholas smith <nicholassmith@nicholass-MacBook-Air.local>
2018-08-24 12:15:47 -0700

    Homepage components: LeftBarMenu,ProductDisplay,SingleProduct,Header

---
+++ HomepageComponents/HeaderComponent.jsx
@@ -0,0 +1,19 @@
+//Dumb Component, will display a logo, cart and user profile link w/ image. 
+import React from 'react'; 
+
+
+
+const HeaderComponent = () => {
+  return (
+    <div> 
+        <div>LOGO HERE</div> 
+        <div>LINK TO profile</div>
+        <div>LOGO FOR CART WITH LINK</div>
+    </div>
+    )
+ }
+
+
+
+
+export module HeaderComponent;  
