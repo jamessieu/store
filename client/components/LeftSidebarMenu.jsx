3f0f9ed857c019c87b7509c7dcae70f4e4c00fcd
Colin Roemer <colin.roemer@gmail.com>
2018-08-24 21:06:38 -0700

    fixing some stuff that broke

--- client/components/LeftSidebarMenu.jsx
+++ client/components/LeftSidebarMenu.jsx
@@ -1,13 +1,12 @@
-
 import React from 'react';
 
 const LeftSidebarMenuComponent = () => {
- return (
- 	<div>
-	<h1>MEN'S LINK</h1>
-	<h1>WOMEN"S LINK</h1>
-	</div>
- )
+   return (
+   	<div>
+    	<h1>MEN'S LINK</h1>
+    	<h1>WOMEN"S LINK</h1>
+  	</div>
+   )
 }
 
 export default LeftSidebarMenuComponent;
--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -2,61 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import HeaderComponent from '../components/HeaderComponent.jsx';
 import Chat from '../components/CustomerService.jsx';
-import HeaderComponent from '../components/HeaderComponent.jsx'
-import ProductDisplay from '../components/ProductDisplayComponent.jsx' 
-import LeftSidebarMenuComponent from '../components/LeftSidebarMenuComponent.jsx'
+import ProductDisplay from '../components/ProductDisplayComponent.jsx';
+// import LeftSidebarMenuComponent from '../components/LeftSidebarMenuComponent.jsx';
 import axios from 'axios'; 
-
-
-const URL = 'https://localhost3000.com'
-
-
-
-
 
 class MainContainer extends Component {
   constructor(props) {
     super(props);
-
-  render() {
-    return (
-      <div> 
-        H3llo from react MainContainer
-        <HeaderComponent />
-        <Chat/>
-      </div>
+  }
+render() {
+  return (
+    <div> 
+      <Chat/>
+      <HeaderComponent />
+      <ProductDisplay /> 
+    </div>
     );
-      images: []
-
-    }
   }
 }
 
 
-axios.get(API_URI) 
-  .then((response) => {
-    const items = response.
-  
-  
-
-}); 
-
-
-
-render() {
-  return (
-    <div>
-      <HeaderComponent />
-      <ProductDisplay /> 
-      <LeftSidebarMenuComponent />
-    </div>
-  );
-}
+// axios.get(API_URI) 
+//   .then((response) => {
+//     const items = response;
+//   });
 
 
 
 export default MainContainer;
-
-
-
-import React from 'react';
