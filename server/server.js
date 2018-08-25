4e5ca62278fe84f02ba6e28e825571d7e5b03a3b
Colin Roemer <colin.roemer@gmail.com>
2018-08-25 12:30:43 -0700

    fixed styling of customer serv chat thing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const db = require('./db/postgresql.js');
-const eventController = require('./db/event-controller.js');
+// const eventController = require('./db/event-controller.js');
 const http = require('http');
 const socket = require('socket.io');
 
@@ -11,17 +11,17 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/main',
-    eventController.getAllProducts,
-)
+// app.get('/main',
+//     eventController.getAllProducts,
+// )
 
-app.get('/mens',
-    eventController.filterByMen,
-)
+// app.get('/mens',
+//     eventController.filterByMen,
+// )
 
-app.get('/womens',
-    eventController.filterByWomen,
-)
+// app.get('/womens',
+//     eventController.filterByWomen,
+// )
 
 
 server = app.listen(PORT, console.log(`Listening on port: ${PORT} ==> this is so tight`));
@@ -29,10 +29,7 @@ server = app.listen(PORT, console.log(`Listening on port: ${PORT} ==> this is so
 const io = socket(server);
 
 io.on('connection', (socket) => {
-    console.log(socket.id);
-
     socket.on('SEND_MESSAGE', function(data){
-      console.log(data);
         io.emit('RECEIVE_MESSAGE', data);
     })
 });
