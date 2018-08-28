77a609112aebc600702ed148adac68dbd9a88b59
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-28 15:06:58 -0700

    cart is made woo

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App.jsx';
 import Login from './Login.jsx';
+import Checkout from './containers/Checkout.jsx'
 import store from './store';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
@@ -37,6 +38,7 @@ const Index = () => {
         <Switch>
           <Route exact path='/' render={() => (<App />)}/>
           <Route path='/login' render={() => (<Login/>)} />
+          <Route path='/cart' render={() => (<Checkout/>)} />
         </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
